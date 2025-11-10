a2fcf3cc4522ee6f7b66dfded22497620809da88
Gether0 <114643703+Gether0@users.noreply.github.com>
2025-11-10 07:17:13 +0000

    task 1 real

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -5,45 +5,42 @@ namespace A1_ZweiThreadsZaehlenWinner;
 
 class Program
 {
-    int counterUp = 0;
-        int counterDown = 0;
-            
-                Thread threadA = new Thread CountUpThreadA();
-                    Thread threadB = new Thread CountDownThreadB();
+    static int counterUp = 0;
+    static int counterDown = 0;
 
-                        threadA.Start();
-                            threadB.Start();
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
 
-                                threadA.Join();
-                                    threadB.Join();
+        Thread threadA = new Thread(() => CountUpThreadA());
+        Thread threadB = new Thread(() => CountDownThreadB());
 
-                                       
-                                           
-                                               public static void Main(string[] args)
-                                                   {
-                                                           Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
-                                                                   
-                                                                           
-                                                                               }
-                                                                                   
-                                                                                       private static void CountUpThreadA()
-                                                                                           {
-                                                                                                   for(int i = 1; i <= 100; i++)
-                                                                                                           {
-                                                                                                                       Thread.Sleep(100);
-                                                                                                                                   counterUp++;
-                                                                                                                                           }
-                                                                                                                                                   
-                                                                                                                                                       }
-                                                                                                                                                           
-                                                                                                                                                               private static void CountDownThreadB()
-                                                                                                                                                                   {
-                                                                                                                                                                           for(int i = 100; i >= 1; i++)
-                                                                                                                                                                                   {
-                                                                                                                                                                                               Thread.Sleep(100);
-                                                                                                                                                                                                           counterDown--;
-                                                                                                                                                                                                                   }
-                                                                                                                                                                                                                          
-                                                                                                                                                                                                                              }
-                                                                                                                                                                                                                              }
-                                                                                                                                                                                                                              
+        threadA.Start();
+        threadB.Start();
+
+        threadA.Join();
+        threadB.Join();
+ 
+
+    }
+
+    private static void CountUpThreadA()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            Thread.Sleep(100);
+            counterUp++;
+        }
+
+    }
+
+    private static void CountDownThreadB()
+    {
+        for (int i = 100; i >= 1; i++)
+        {
+            Thread.Sleep(100);
+            counterDown = i;
+        }
+
+    }
+}
